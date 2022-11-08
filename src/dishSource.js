826492b36b5610e931f1807de665fda534964f3f
Lady Geraldine Salazar Bayona <lgsb@kth.se>
2022-11-08 14:14:34 +0100

    modifications to the project TW2.2 getDishDetails and searchDishes, working

---
+++ src/dishSource.js
@@ -0,0 +1,50 @@
+export { getDishDetails, searchDishes };
+import { BASE_URL, API_KEY } from "/src/apiConfig.js";
+
+function treatHTTPResponseACB(response) {
+    /*TODO throw if the HTTP response is not 200, otherwise return response.json()*/
+    if (response === 200) {
+        throw "API problem";
+    } else {
+        return response.json();
+
+    }
+}
+
+
+async function getDishDetails(params) {
+    const endpoint = "recipes/";
+    return fetch(BASE_URL + endpoint + params.toString() + "/information", {  // object literal
+        "method": "GET",              // HTTP method
+        "headers": {                  // HTTP headers, also object literal
+            'X-Mashape-Key': API_KEY,
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        } // end of headers object
+    }/* end of second fetch parameter, object */
+    )
+        .then(treatHTTPResponseACB);
+
+
+
+}
+function searchDishes(params) {
+    const endpoint = "recipes/search?";
+    function transformResultACB(object) {
+        return Object.values(object);
+       }
+
+    return fetch(BASE_URL + endpoint+ new URLSearchParams(params), {  // object literal
+        "method": "GET",              // HTTP method
+        "headers": {                  // HTTP headers, also object literal
+            'X-Mashape-Key': API_KEY,
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        } // end of headers object
+    }/* end of second fetch parameter, object */
+    )
+        .then(treatHTTPResponseACB)
+        .then(transformResultACB);
+
+    /*TODO throw if the HTTP response is not 200, otherwise return response.json()*/
+}
+
+
