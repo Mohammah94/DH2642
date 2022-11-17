f7e18f422592ac720b3c6b0e6734b4e875d8e7eb
Lady Geraldine Salazar Bayona <lgsb@kth.se>
2022-11-17 19:19:59 +0100

    modification TW3.2 Vue stateful Search presenter

--- src/vuejs/searchPresenter.js
+++ src/vuejs/searchPresenter.js
@@ -1,7 +1,55 @@
 import SearchResultsView from "../views/searchResultsView";
 import SearchFormView from "../views/searchFormView.js";
 import promiseNoData from "../views/promiseNoData";
+import { searchDishes} from "../dishSource";
+import ResolvePromise from "../resolvePromise";
+const Search = {   // ordinary JS object literal, can have methods like render()
+  props: ["model"],
+  data() { return {query:"", type:"",promiseState:{}} },
+  created() {
+    const component = this;
+    ResolvePromise(searchDishes({query: component.query, type: component.type}),
+    component.promiseState);
 
+  },
+  render() {
+    const component = this;
+    /* re-use the TW2 functional component code, but replace props with component! */
+    function searchFromMenuACB() {
+      //component.model.doSearch({query: component.query, type: component.type})
+      ResolvePromise(searchDishes({query: component.query, type: component.type}),component.promiseState);
+    }
+    function textSearchMenuACB(query) {
+      component.query=query;
+    }
+  
+    function typeSearchMenuACB(type) {
+      component.type=type;
+    }
+    function onClickACB(res) {
+      component.model.setCurrentDish(res.id)
+     } 
+     return (
+      <div>
+          <SearchFormView
+              dishTypeOptions={["starter", "main course", "dessert"]}
+              searchFromMenu={searchFromMenuACB}
+              searchText={textSearchMenuACB}
+              searchType={typeSearchMenuACB}
+          />
+  
+          { promiseNoData(this.promiseState)|| 
+          <SearchResultsView onResultClick={onClickACB} 
+          searchResults={this.promiseState.data} />}
+      </div>
+  );
+  },
+
+  
+};
+export default Search;
+
+/*
 export default function searchPresenter(props) {
     
 if (!props.model.searchResultsPromiseState.promise ){
@@ -35,4 +83,4 @@ return (
     </div>
 );
 
-}
+}*/
