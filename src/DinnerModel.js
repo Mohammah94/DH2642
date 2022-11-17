53eb916ff1df89c56cbe61bb299b05044407b2a9
Lady Geraldine Salazar Bayona <lgsb@kth.se>
2022-11-17 16:35:19 +0100

    modification TW3.1 DinnerModel as Observable and DinnerModel notifies its observers

--- src/DinnerModel.js
+++ src/DinnerModel.js
@@ -2,81 +2,124 @@
    The Model keeps only abstract data and has no notions of graohics or interaction
 */
 import ResolvePromise from "./resolvePromise";
-import {searchDishes, getDishDetails} from "./dishSource";
-class DinnerModel{
-    constructor(nrGuests=2, dishArray=[], currentDish){
-        this.searchResultsPromiseState= {};
-        this.searchParams= {};
-        this.currentDishPromiseState ={};
+import { searchDishes, getDishDetails } from "./dishSource";
 
+class DinnerModel {
+
+    constructor(nrGuests = 2, dishArray = [], currentDish) {
+        this.observers = [];
+        this.searchResultsPromiseState = {};
+        this.searchParams = {};
+        this.currentDishPromiseState = {};
         this.setNumberOfGuests(nrGuests);
-        this.dishes= dishArray;
+        this.dishes = dishArray;
     }
-    setNumberOfGuests(nr){
+    /*TW3.1 DinnerModel as Observable*/
+    addObserver(callback) {
+        this.observers.push(callback);
+    }
 
-        if (nr > 0 && Number.isInteger(nr)) {
-            this.numberOfGuests = nr;
-          }
-          else {
-            throw "number of guests not a positive integer";
-          }        
-        
-        // if() and throw exercise
-        
-        // TODO throw an error if the argument is smaller than 1 or not an integer
-        // the error message must be exactly "number of guests not a positive integer"
-        // to check for integer: test at the console Number.isInteger(3.14)
+    removeObserver(callback) {
+        function checkCallbackCB(check) { return callback !== check }
+        this.observers = this.observers.filter(checkCallbackCB);
+    }
 
-        // TODO if the argument is a valid number of guests, store it in this.numberOfGuests
-        
-        // when this is done the TW1.1 DinnerModel "can set the number of guests" should pass
-        // also "number of guests is a positive integer"
-    }
-    addToMenu(dishToAdd){
-        // array spread syntax example. Make sure you understand the code below.
-        // It sets this.dishes to a new array [   ] where we spread (...) the previous value
-        this.dishes= [...this.dishes, dishToAdd];
-    }
-    
-    removeFromMenu(dishToRemove){
-        // callback exercise! Also return keyword exercise
-        function hasSameIdCB(dish){
-            if (dish["id"] !== dishToRemove["id"]){
-                return true
-            }else{
-                return false
+    notifyObservers(payload) {
+        this.observers.forEach(function invokeObserverCB(obs) {
+            try {
+                obs(payload);
+            } catch (err) {
+                console.error(err);
             }
-            // TODO return true if the id property of dish is _different_ from the dishToRemove's id property
-            // This will keep the dish when we filter below.
-            // That is, we will not keep the dish that has the same id as dishToRemove (if any)
-        }
-        this.dishes= this.dishes.filter(hasSameIdCB);
-        // the test "can remove dishes" should pass
+        });
     }
-    /* 
-       ID of dish currently checked by the user.
-       A strict MVC/MVP Model would not keep such dgooata, 
-       but we take a more relaxed, "Application state" approach. 
-       So we store also abstract data that will influence the application status.
-     */
-    setCurrentDish(id){
-        
-        if (id!== undefined && this.currentDish !== id  ){
-            this.currentDish = id;
-        ResolvePromise(getDishDetails(this.currentDish),this.currentDishPromiseState)}
+    /*__________________________________________________________________________ */
+
+    setNumberOfGuests(nr) {
+        if (nr !== this.numberOfGuests) {
+            if (nr > 0 && Number.isInteger(nr)) {
+                this.numberOfGuests = nr;
+                this.notifyObservers({ guests: nr });
+            }
+            else {
+                throw "number of guests not a positive integer";
+            }
+        }
+    }
+
+
+    addToMenu(dishToAdd) {
+        if (!evaluateDshId(this.dishes, dishToAdd.id)) {
+            this.dishes = [...this.dishes, dishToAdd];
+            this.notifyObservers({ addDish: dishToAdd });
+        }
 
     }
-    setSearchQuery(q){
-        this.searchParams.query=q;
+
+
+    removeFromMenu(dishToRemove) {
+        function hasSameIdCB(dish) {
+
+            if (dish.id === dishToRemove.id) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (evaluateDshId(this.dishes, dishToRemove.id)) {
+            this.dishes = this.dishes.filter(hasSameIdCB);
+            this.notifyObservers({ removeDish: dishToRemove });
+        }
     }
-    setSearchType(t){
-        this.searchParams.type=t;
+
+
+    setCurrentDish(id) {
+        if (id !== undefined && this.currentDish !== id) {
+            this.currentDish = id;
+            const model = this;
+            function notifyACB() {
+                model.notifyObservers();
+            }
+            ResolvePromise(getDishDetails(this.currentDish), this.currentDishPromiseState, notifyACB);
+            model.notifyObservers({ currentDish: id });
+        }
     }
-    doSearch(params){
+
+
+    setSearchQuery(q) {
+        this.searchParams.query = q;
+        this.notifyObservers({ searchType: q });
+    }
+
+
+    setSearchType(t) {
+        this.searchParams.type = t;
+        this.notifyObservers({ searchType: t });
+    }
+
+
+    doSearch(params) {
         this.searchParams = params;
-        ResolvePromise(searchDishes(this.searchParams),this.searchResultsPromiseState)
-    } 
+        const model = this;
+        function notifyACB() {
+            model.notifyObservers();
+        }
+        ResolvePromise(searchDishes(this.searchParams), this.searchResultsPromiseState, notifyACB);
+    }
 
 }
-
+function evaluateDshId(dishes, currentId) {
+    const id = dishes.filter(function checkIfDishExistCB(dish) {
+        if (dish.id === currentId) {
+            return true;
+        } else {
+            return false;
+        }
+    })
+    if (id[0] === undefined) {
+        return false;
+    } else {
+        return true;
+    }
+}
 export default DinnerModel;
